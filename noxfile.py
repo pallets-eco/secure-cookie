d715bd4449feda0513e8d4a0056cbd173943e89b
David Lord <davidism@gmail.com>
2020-02-25 15:27:46 -0800

    use nox and github workflows

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+import nox
+
+
+@nox.session(python=["3.8", "3.7", "3.6", "3.5", "2.7"])
+def tests(session):
+    if session.python == "2.7":
+        # unpinned on 2.7 so pip will find the last supported versions.
+        session.install(".", "-r", "requirements/tests.in")
+    else:
+        session.install(".", "-r", "requirements/tests.txt")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session()
+def style(session):
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files")
+
+
+@nox.session()
+def docs(session):
+    session.install(".", "-r", "requirements/docs.txt")
+    session.run("sphinx-build", "-M", "clean", "docs", "docs/_build")
+    session.run("sphinx-build", "-M", "html", "docs", "docs/_build", "-W")
