e1e530a48a6cc8278a69578a7334b24658bba493
Stéphane Blondon <stephane.blondon@gmail.com>
2020-04-04 10:28:24 +0200

    minimal version for itsdangerous dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(name="secure-cookie", install_requires=["Werkzeug>0.15", "itsdangerous"])
+setup(name="secure-cookie", install_requires=["Werkzeug>0.15", "itsdangerous>=1.1"])
